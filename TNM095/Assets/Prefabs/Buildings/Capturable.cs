962a852ee9a30563629b3ee956296bbfdd726ec5
Erik <e.naaru@hotmail.com>
2019-09-06 13:26:52 +0200

    Add initial Unity project with main scene and player/castle prefabs

---
+++ TNM095/Assets/Prefabs/Buildings/Capturable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capturable : MonoBehaviour {
+    /// <summary>
+    /// <see cref="Player"/> instance that currently owns this <see cref="Capturable"/>.
+    /// </summary>
+    public Player owner;
+    
+    /// <summary>
+    /// List of Renderers that should change their material to that of the owning player to indicate ownership¨.
+    /// </summary>
+    public List<Renderer> ownerIndicators;
+
+    private int unitCount;
+
+    void Start()
+    {
+        if (owner != null)
+        {
+            foreach (Renderer r in ownerIndicators)
+            {
+                r.material = owner.playerMaterial;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ TNM095/Assets/Prefabs/Player/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public Material playerMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
