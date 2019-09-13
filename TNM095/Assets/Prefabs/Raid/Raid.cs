5ae77b83924cbb8a7c79f094106aacfebe8f3866
Sebson <sirmcmuffin95@gmail.com>
2019-09-13 19:03:28 +0200

    Added moving for raidunits

--- TNM095/Assets/Prefabs/Raid/Raid.cs
+++ TNM095/Assets/Prefabs/Raid/Raid.cs
@@ -3,9 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Raid : MonoBehaviour{
+  private int unitCount = 200;
+  private List<GameObject> raiders;
+  private int spawnNumber;
+  private Vector3 movVec;
+
+  public Capturable dest;
+  public float circleSize;
+  public float speed;
+  public GameObject raiderPrefab;
+  public UnitIndicator indicator;
+  public Player owner;
+
   void Start() {
+    spawnNumber = (int)Mathf.Floor(unitCount/Mathf.Sqrt(unitCount));
+    raiders = new List<GameObject>();
+    movVec = Vector3.Normalize(dest.transform.position - transform.position);
+    indicator.UpdateText(unitCount.ToString(), owner.playerColor);
+
+    for(int i=0; i < spawnNumber; i++) {
+      Vector2 randPos = circleSize*Random.insideUnitCircle;
+      raiders.Add(Instantiate<GameObject>(raiderPrefab, new Vector3(transform.position.x+randPos.x,transform.position.y,transform.position.z+randPos.y), Quaternion.identity, transform));
+    }
   }
 
   void Update() {
+    Move();
+  }
+
+  public void Init(int units) {
+    unitCount = units;
+  }
+
+  private void Move() {
+    transform.position += movVec*speed*Time.deltaTime;
+    if((transform.position - dest.transform.position).magnitude < 0.1)
+      Debug.Log("Honey we are hooooome!");//Capturable.Raided();
   }
 }
